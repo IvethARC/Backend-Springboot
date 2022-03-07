671e41cc8ede170b93a4b070fbe81bac7db54cfa
Iveth Rodríguez <ivethrodriguezmath@gmail.com>
2022-03-06 23:24:14 -0500

    Backend terminado con conexion a la db.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/72/306d34f25197001c146b955dd5bb4bae
@@ -0,0 +1,43 @@
+package com.reto.wishlist.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.reto.wishlist.entity.Product;
+import com.reto.wishlist.repository.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+	
+	@Autowired
+	private ProductRepository productRepository;
+
+	@Override
+	@Transactional(readOnly=true)
+	public Iterable<Product> findAll() {
+		return productRepository.findAll();
+	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public Optional<Product> findById(Long id) {
+		return productRepository.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public Product save(Product product) {
+		return productRepository.save(product);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(Long id) {
+		productRepository.deleteById(id);
+		
+	}
+
+}
---
+++ wishlist/src/main/java/com/reto/wishlist/entity/Product.java
@@ -0,0 +1,63 @@
+package com.reto.wishlist.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+	
+	@Id
+	private Long id;
+	
+	private String name;
+	private Long price;
+	private Long amount;
+	private String information;
+	private String image_url;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getPrice() {
+		return price;
+	}
+	public void setPrice(Long price) {
+		this.price = price;
+	}
+	public Long getAmount() {
+		return amount;
+	}
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+	public String getInformation() {
+		return information;
+	}
+	public void setInformation(String information) {
+		this.information = information;
+	}
+	public String getImage_url() {
+		return image_url;
+	}
+	public void setImage_url(String image_url) {
+		this.image_url = image_url;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
